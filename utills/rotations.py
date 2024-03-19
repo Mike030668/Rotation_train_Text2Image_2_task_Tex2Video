e80a15170a4e33c479559735ac53b946e59e6c18
Михаил Пузицкий <gretawolf@live.ru>
2024-03-19 17:02:55 +0300

    Merge branch 'main' of https://github.com/Mike030668/Rotation_tran_Text2Image_2_task_Tex2Video into main

--- utills/rotations.py
+++ utills/rotations.py
@@ -45,4 +45,4 @@ class RotationVectors:
          Rotate batch_vectors as matrix with batch R_matrixes
          with using batch cos_sim these batch_vectors to engine vectors
          """
-         return torch.mul((1 - cos_sim_matrix**power).unsqueeze(1), b_matrix) + torch.mul(cos_sim_matrix**power.unsqueeze(1), b_matrix @ b_R_matrix)
+         return torch.mul(1 - torch.pow(cos_sim_matrix,power).unsqueeze(1), b_matrix) + torch.mul(torch.pow(cos_sim_matrix,power).unsqueeze(1), b_matrix @ b_R_matrix)
